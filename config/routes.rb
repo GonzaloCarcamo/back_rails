61de9a1d0d47b5c36697dafdc9a3d497d5cdb144
GonzaloCarcamo <gon.cmo.dev@gmail.com>
2022-02-17 17:38:49 -0300

    main route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :difficulties
   resources :categories
   resources :projects
+
+  root to: "projects#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
